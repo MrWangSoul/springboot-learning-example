88ca2b9fcd9610c23eb40db3743f15c2d704767a
MrWang <1084697634@qq.com>
2018-07-14 20:03:56 +0800

    添加注解

--- springboot-mybatis/src/main/java/org/spring/springboot/dao/UserMapper.java
+++ springboot-mybatis/src/main/java/org/spring/springboot/dao/UserMapper.java
@@ -1,6 +1,9 @@
 package org.spring.springboot.dao;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.spring.springboot.domain.User;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
@@ -10,6 +13,7 @@ import java.util.List;
  * @author: Mr.Wang
  * @create: 2018-07-04 17:06
  **/
+//@Mapper 1.在启动类上面使用MapperScan后直接扫描  后面的mapper上面全都不用写Mapper注解了
 public interface UserMapper {
 
     List<User> getUser();
